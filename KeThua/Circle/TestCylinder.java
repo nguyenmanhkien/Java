e85dbc0eda2b55658769aa40df10abda54d7e241
nguyenmanhkien <nguyenmanhkien290103@gmail.com>
2022-04-11 21:36:45 +0700

    .

---
+++ KeThua/Circle/TestCylinder.java
@@ -0,0 +1,17 @@
+package KeThua.Circle;
+
+public class TestCylinder {
+    public static void main(String[] args) {
+        Cylinder cy1 = new Cylinder();
+        System.out.println("Radius is "+ cy1.getRadius()
+        + ",Height is "+ cy1.getHeight()
+        + ",Color is "+ cy1.getArea()
+        + ",Base area is "+ cy1.getArea());
+
+        Cylinder cy2 = new Cylinder();
+        System.out.println("Radius is "+ cy2.getRadius()
+                + ",Height is "+ cy2.getHeight()
+                + ",Color is "+ cy2.getArea()
+                + ",Base area is "+ cy2.getArea());
+    }
+}
