9597d1c2100293da41fd47ce444412481c67daac
nguyenmanhkien <nguyenmanhkien290103@gmail.com>
2022-04-13 21:39:27 +0700

    .

---
+++ Polymorphism/Circle/Cylinder.java
@@ -0,0 +1,24 @@
+package Polymorphism.Circle;
+
+public class Cylinder extends Circle{
+    public double height;
+    public Cylinder(double height, double radius){
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return super.getArea()*height;
+    }
+    @Override
+    public double getArea(){
+        return 2.0*Math.PI*getRadius()*height;
+    }
+    @Override
+    public String toString(){
+        return "Cylinder [ height = "+height + "," +super.toString()+"]";
+    }
+}
---
+++ Polymorphism/Circle/TestCylinder.java
@@ -0,0 +1,8 @@
+package Polymorphism.Circle;
+
+public class TestCylinder {
+    public static void main(String[] args) {
+        Circle c1 = new Cylinder(1.1,2.2);
+        System.out.println(c1.getRadius());
+    }
+}
